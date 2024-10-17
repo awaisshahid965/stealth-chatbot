8c9d5d12ce3dd01e78e98e41e275e9c6838d3167
Muhammad Awais <awaisshahid965@gmail.com>
2024-10-17 06:03:50 -0700

    chore: added error handling

--- backend/app/controllers/chat.py
+++ backend/app/controllers/chat.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import StreamingResponse
 from langchain.chains import create_retrieval_chain
 from langchain_core.runnables import RunnablePassthrough
@@ -7,6 +7,8 @@ from app.vector_stores.qdrant_vector_store import QdrantVectoreStore
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
+
+from typing import AsyncGenerator
 
 
 from dotenv import load_dotenv
@@ -33,14 +35,23 @@ combine_docs_chain = create_stuff_documents_chain(model, retrieval_qa_chat_promp
 rag_chain = create_retrieval_chain(qdrant_vector_store.get_retriever(), combine_docs_chain)
 
 async def answer_stream(query: str):
-    for chunk in rag_chain.stream({"input": query}):
-        if answer_chunk := chunk.get("answer"):
-            yield f"{answer_chunk}\n"
+    try:
+        for chunk in rag_chain.stream({"input": query}):
+            if answer_chunk := chunk.get("answer"):
+                yield f"{answer_chunk}\n"
+    except Exception as e:
+        raise RuntimeError(f"Error while streaming: {str(e)}")
 
 @router.post("/query/")
 async def query_endpoint(request: Request):
     body = await request.json()
     query = body.get("query")
-    
-    return StreamingResponse(answer_stream(query), media_type="text/plain")
+
+    if not query:
+        raise HTTPException(status_code=400, detail="Query not provided")
+
+    try:
+        return StreamingResponse(answer_stream(query), media_type="text/plain")
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
